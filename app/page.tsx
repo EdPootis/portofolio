28c71c36ef9614bfb2af63f8157d007555bcf624
Edmond Christian <edmond.chr@gmail.com>
2026-06-14 21:33:33 +0700

    fix: changed portofolio preview links to github

--- app/page.tsx
+++ app/page.tsx
@@ -478,11 +478,11 @@ export default function Home() {
                 <div className="flex justify-between items-start gap-2 mb-1">
                   <h4 className="text-2xl font-bold text-white">Personal Portfolio</h4>
                   <a
-                    href="https://edpootis.vercel.app/"
+                    href="https://github.com/edpootis/portofolio"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-gray-400 hover:text-neon-cyan transition-colors mt-1 shrink-0 cursor-pointer"
-                    title="Live Website"
+                    title="GitHub Repository"
                   >
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" /></svg>
                   </a>
